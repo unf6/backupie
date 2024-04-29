0dbac9dd20afaad3099d5f14968a20ae76d31cf3
Sadness <95174481+unf6@users.noreply.github.com>
2024-04-30 00:02:34 +0300

    Create AutomodData.ts

---
+++ src/types/AutomodData.ts
@@ -0,0 +1,34 @@
+import { Snowflake, AutoModerationActionType, AutoModerationActionMetadataData, AutoModerationRuleTriggerType, AutoModerationRuleEventType, AutoModerationTriggerMetadata } from 'discord.js';
+
+export interface ExemptRoleData {
+    id: string;
+    name: string;
+}
+
+export interface ExemptChannelData {
+    id: string;
+    name: string;
+}
+
+export interface AutoModerationActionMetadataData {
+    channelId?: Snowflake;
+    channelName?: string;
+    durationSeconds?: number;
+    customMessage?: string;
+}
+
+export interface AutoModRuleActionData {
+    type: AutoModerationActionType;
+    metadata: AutoModerationActionMetadataData;
+}
+
+export interface AutoModRuleData {
+    name: string;
+    eventType: AutoModerationRuleEventType;
+    triggerType: AutoModerationRuleTriggerType;
+    triggerMetadata: AutoModerationTriggerMetadata;
+    actions: AutoModRuleActionData[];
+    enabled: boolean;
+    exemptRoles: ExemptRoleData[],
+    exemptChannels: ExemptChannelData[],
+}
